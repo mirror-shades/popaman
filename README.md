77aac522320c7e303e8d9810f0ab86d2e1016a68
mirror-shades <44949816+mirror-shades@users.noreply.github.com>
2024-10-30 18:27:11 -0700

    started building install functionality

--- README.md
+++ README.md
@@ -7,6 +7,42 @@
    ```
 
 2. Install Portman:
+
    ```
    ./install-portman.exe
    ```
+
+   To install a package to the global list, use the install command with the `-g` flag:
+
+   ```
+   portman install <package> -g
+   ```
+
+3. Global Packages:
+   Global packages are available to all users on the system. They are accessed by adding a .sh file to the `portman/bin` directory which is in the PATH.
+   If a package wasn't installed globally it can be still be added to the global list using the global command with the `-a` flag:
+
+   ```
+   portman global <package> -a
+   ```
+
+   Or remove an existing package from the global list, use the global command with the `-r` flag:
+
+   ```
+   portman global <package> -r
+   ```
+
+4. Package Format:
+   The `packages.json` file uses the following format for each package:
+
+   ```json
+   {
+     "name": "directory_name", // Name of the package directory
+     "path": "executable.exe", // Path to the executable
+     "keyword": "keyword_to_use", // Command keyword to invoke the package
+     "global": true, // Whether package is globally available
+     "description": "optional description" // Description of the package
+   }
+   ```
+
+   This defines how Portman locates and manages packages in the system.
--- src/run.zig
+++ src/run.zig
@@ -16,6 +16,22 @@ const Package = struct {
     description: []const u8,
 };
 
+fn readLineFixed() ![]const u8 {
+    var buffer: [240]u8 = undefined;
+    const stdin = std.io.getStdIn();
+    var buffered = std.io.bufferedReader(stdin.reader());
+    var reader = buffered.reader();
+    
+    if (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        // Make a copy of the trimmed input
+        const trimmed = std.mem.trim(u8, line, &[_]u8{ '\r', '\n', ' ', '\t' });
+        // Return just the valid part of the input
+        return trimmed[0..trimmed.len];
+    }
+    
+    return error.EndOfStream;
+}
+
 fn parse_package_info(allocator: std.mem.Allocator, keyword: []const u8) !?Package {
     // Create buffer for executable path
     var exe_dir_buf: [std.fs.max_path_bytes]u8 = undefined;
@@ -94,16 +110,112 @@ fn get_packages(allocator: std.mem.Allocator) ![][]const u8 {
     return keywords;
 }
 
+fn install_package(allocator: std.mem.Allocator, package: []const u8, is_global: bool) !void {
+    
+
+    std.debug.print("Enter the keyword for the package: ", .{});
+    const keyword = try readLineFixed();
+    const keyword_copy = try allocator.dupe(u8, keyword);
+    defer allocator.free(keyword_copy);
+
+    std.debug.print("Enter the description for the package: ", .{});
+    const description = try readLineFixed();
+    const desc_copy = try allocator.dupe(u8, description);
+    defer allocator.free(desc_copy);
+    
+    if (is_global) {
+        std.debug.print("Installing package and adding to global list: {s}\n", .{package});
+    } else {
+        std.debug.print("Installing package: {s}\n", .{package});
+    }
+
+    //name: package
+    //keyword: keyword_copy
+    //description: desc_copy
+    //global: is_global
+}
+
+fn globalize_package(allocator: std.mem.Allocator, package: []const u8, is_add: bool) !void {
+    _ = allocator;
+    if (is_add) {
+        std.debug.print("Adding package to global list: {s}\n", .{package});
+    } else {
+        std.debug.print("Removing package from global list: {s}\n", .{package});
+    }
+}
+
+fn remove_package(allocator: std.mem.Allocator, package: []const u8) !void {
+    _ = allocator;
+    std.debug.print("Removing package: {s}\n", .{package});
+}
+
+
 pub fn run_portman() !void {
     std.debug.print("Running Portman...\n", .{});
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
+
+    // Get command line arguments
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
     
-    const keywords = try get_packages(allocator);
-    // No need to manually free keywords as arena allocator will handle it
-    
-    for (keywords) |keyword| {
-        std.debug.print("Keyword: {s}\n", .{keyword});
+    // Skip the executable name
+    _ = args.skip();
+
+    // Check if there are any arguments
+    if (args.next()) |command| {
+        if (std.mem.eql(u8, command, "install")) {
+            if (args.next()) |package| {
+                var is_global = false;
+                // Check for -g flag
+                if (args.next()) |flag| {
+                    if (std.mem.eql(u8, flag, "-g")) {
+                        is_global = true;
+                    }
+                }
+                try install_package(allocator, package, is_global);
+            } else {
+                std.debug.print("Error: Package path is required\n", .{});
+                std.debug.print("Usage: portman install <package path> [-g]\n", .{});
+                return;
+            }
+        } else if (std.mem.eql(u8, command, "global")) {
+            if (args.next()) |package| {
+                if (args.next()) |flag| {
+                    if (std.mem.eql(u8, flag, "-a")) {
+                        try globalize_package(allocator, package, true);
+                    }
+                    else if (std.mem.eql(u8, flag, "-r")) {
+                        try globalize_package(allocator, package, false);
+                    }
+                    else {
+                        std.debug.print("Use -a or -r to add or remove\n", .{});
+                        std.debug.print("portman global <package> -a\n", .{});
+                        std.debug.print("portman global <package> -r\n", .{});
+                        return;
+                    }
+                }
+            }
+        } else if (std.mem.eql(u8, command, "remove")) {
+            if (args.next()) |package| {
+                try remove_package(allocator, package);
+            }
+        } else if (std.mem.eql(u8, command, "list")) {
+            const keywords = try get_packages(allocator);
+            for (keywords) |keyword| {
+                std.debug.print("Available package: {s}\n", .{keyword});
+            }
+        }
+    } else {
+        // No arguments provided, show help
+        std.debug.print("Usage: portman <command> [options]\n", .{});
+        std.debug.print("Commands:\n", .{});
+        std.debug.print("  install <package>     Install a package\n", .{});
+        std.debug.print("  install <package> -g  Install a package globally\n", .{});
+        std.debug.print("  global <package> -a   Add package to global list\n", .{});
+        std.debug.print("  global <package> -r   Remove package from global list\n", .{});
+        std.debug.print("  remove <package>      Remove a package\n", .{});
+        std.debug.print("  list                  List all available packages\n", .{});
     }
 }
